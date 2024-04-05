04f24a172839d0daac748e44d285e5bb0f97596b
prath7972 <prathmeshmundada07@gmail.com>
2024-04-05 21:50:12 +0530

    hello
    hello

---
+++ script.js
@@ -0,0 +1,66 @@
+//console.log("I am a student");
+
+//var name=prompt("i am taking input")
+//console.log(name);
+
+//for(let i=1;i<=10;i++){
+    //console.log(i*2);
+//}
+
+
+
+// let n=1;
+// while(n<=10){
+//    console.log(n);
+//     n++;
+// }
+
+
+
+// let n=prompt("give me number")
+// if(n%2==0){
+//     console.log("I am even");
+// }
+
+// else{
+//     console.log("I am odd");
+// }
+
+
+
+// let marks=prompt("Enter your marks");
+// if(marks>80 && marks<90){
+//     console.log("A grade");
+// } 
+
+// else if(marks>70 && marks<80){
+//     console.log("B grade");
+// }
+
+// else if(marks>60 && marks<70){
+//     console.log("B grade");
+// }
+
+// else{
+//     console.log("D grade");
+// }
+
+
+// let fname=prompt("Give fname");
+// let lname=prompt("Give lname");
+// let msg="Hello " +fname +"  " +lname+" you are using JS!!"
+// alert(msg);
+
+
+// const favmovie="Avengers";
+// let guess=prompt("Guess my favourite");
+
+// while((guess!=favmovie) && (guess!="quit")){
+//     guess=prompt("wrong answer!! please try again");
+// }
+// if(guess==favmovie){
+//     console.log("Congrats Buddy")
+// }
+// else{
+//     console.log("you quited")
+// }
